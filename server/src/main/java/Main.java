8a95b7ab87f62de6177acca1d5a00675d0c8657a
Will <willmknisley@gmail.com>
2025-10-22 23:11:05 -0600

    fixed file server issue

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,19 @@
 import chess.*;
-
+/*
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+    }
+}*/
+
+import server.Server;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run(8080);
+
+        System.out.println("♕ 240 Chess Server");
     }
 }
