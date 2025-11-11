6b6ec63f7700c6d633a31da58771ad1588d50ef2
Will <willmknisley@gmail.com>
2025-11-11 00:28:50 -0700

    added help

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -5,4 +5,17 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
     }
+
+    public static void help() {
+        System.out.println("Available commands: ");
+        System.out.println();
+        System.out.println("help - displays this help information");
+        System.out.println();
+        System.out.println("register <username> <password> <email> - create account");
+        System.out.println();
+        System.out.println("login <username> <password> - sign in");
+        System.out.println();
+        System.out.println("quit - exit application");
+    }
+
 }
