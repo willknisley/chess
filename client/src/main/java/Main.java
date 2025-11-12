da847b14077a9bc7691231b57c8ba9bf4c7a78f9
Will <willmknisley@gmail.com>
2025-11-11 22:49:16 -0700

    added preloginui

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import chess.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
@@ -8,14 +10,32 @@ public class Main {
 
     public static void help() {
         System.out.println("Available commands: ");
-        System.out.println();
-        System.out.println("help - displays this help information");
-        System.out.println();
+        System.out.println("help - gives possible commands");
         System.out.println("register <username> <password> <email> - create account");
-        System.out.println();
         System.out.println("login <username> <password> - sign in");
-        System.out.println();
         System.out.println("quit - exit application");
+    }
+
+    public static void preloginUI() {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+
+            System.out.print("[LOGGED_OUT] >>> ");
+            String input = scanner.nextLine();
+            String command = input.trim().toLowerCase();
+
+            if (command.equals("help")) {
+                help();
+            } else if (command.equals("quit")) {
+                System.out.println("exiting application");
+                running = false;
+            } else if (!command.isEmpty()) {
+                System.out.println("Unknown command. Type 'help' for available commands.");
+            }
+        }
+
+        scanner.close();
     }
 
 }
