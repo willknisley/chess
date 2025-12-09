d578cd3543ded4be311d05ea5b32f22914d6ff6a
Will <willmknisley@gmail.com>
2025-12-09 00:49:05 -0700

    fixed loadgamemessage

--- shared/src/main/java/websocket/messages/LoadGameMessage.java
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -4,8 +4,13 @@ import model.GameData;
 
 public class LoadGameMessage extends ServerMessage {
     private final GameData game;
+
     public LoadGameMessage(GameData game) {
         super(ServerMessageType.LOAD_GAME);
         this.game = game;
+    }
+
+    public GameData getGame() {
+        return game;
     }
 }
